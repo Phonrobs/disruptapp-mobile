73c5f6c8115f3778d0cb6d8887a3558d255a4ea8
phonrob <phonrob.s@up.ac.th>
2016-11-28 03:06:05 +0700

    add ADAL

--- BlankCordovaApp1/www/js/controllers.js
+++ BlankCordovaApp1/www/js/controllers.js
@@ -1,18 +1,36 @@
 ﻿angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function ($scope, UI, AzureBlobService) {
-    $scope.data = {};
+.controller('DashCtrl', function ($scope, UI, AzureBlobService, AzureClient, $ionicModal) {
+    var _client;
+    var _feedTable;
+    var _localPictureUrl;
+    var _addFeedModal;
+    var _readFeedModal;
+
+    $scope.add = function () {
+        $scope.item = {};
+
+        if (!_addFeedModal) {
+            $ionicModal.fromTemplateUrl('add-feed-modal.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function (modal) {
+                _addFeedModal = modal;
+                _addFeedModal.show();
+            });
+        }
+        else
+            _addFeedModal.show();
+    };
 
     $scope.save = function () {
-        var client = new WindowsAzure.MobileServiceClient('https://disruptapp.azurewebsites.net');
-        var problemTable = client.getTable('ProblemItem');
+        UI.startLoading('Saving...');
 
-        UI.startLoading('กำลังบันทึกข้อมูล');
-
-        problemTable.insert($scope.data).then(function (data) {
+        _feedTable.insert($scope.item).then(function (data) {
             AzureBlobService.upload(_localPictureUrl, data.picture, data.sasToken).then(function () {
+                $scope.items.splice(0, 0, data);
                 UI.stopLoading();
-                alert('บันทึกข้อมูลของคุณเรียบร้อยแล้ว');
+                _addFeedModal.hide();
             }, function (err) {
                 UI.stopLoading();
                 UI.error(err);
@@ -23,7 +41,9 @@
         });
     };
 
-    var _localPictureUrl;
+    $scope.cancel = function () {
+        _addFeedModal.hide();
+    };
 
     $scope.takePhoto = function () {
         navigator.camera.getPicture(function (data) {
@@ -34,29 +54,70 @@
         }, function (err) {
         });
     };
+
+    $scope.read = function (item) {
+        $scope.item = item;
+
+        if (!_readFeedModal) {
+            $ionicModal.fromTemplateUrl('read-feed-modal.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function (modal) {
+                _readFeedModal = modal;
+                _readFeedModal.show();
+            });
+        }
+        else
+            _readFeedModal.show();
+    };
+
+    $scope.like = function () {
+        _readFeedModal.hide();
+    };
+
+    $scope.close = function () {
+        _readFeedModal.hide();
+    };
+
+    $scope.refresh = function () {
+        if (!_client) {
+            _client = new WindowsAzure.MobileServiceClient(AzureClient.getServiceUrl());
+            _feedTable = _client.getTable('FeedItem');
+        }
+
+        _feedTable.read().then(function (data) {
+            $scope.items = data;
+        }, function (err) {
+            UI.error(err);
+        });
+    };
+
+    $scope.refresh();
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function ($scope, AzureClient) {
+    //$scope.username = AzureClient.getCurrentUser().displayableId;
+
+    $scope.login = function () {
+        AzureClient.login();
+    };
 });
--- BlankCordovaApp1/www/js/services.js
+++ BlankCordovaApp1/www/js/services.js
@@ -10,6 +10,15 @@ angular.module('starter.services', [])
         console.log(JSON.stringify(msg));
     };
 
+    this.alert = function (msg) {
+        $ionicPopup.alert({
+            title: 'Message',
+            template: msg
+        });
+
+        console.log(JSON.stringify(msg));
+    };
+
     this.startLoading = function (msg) {
         if (!msg)
             msg = 'Loading...';
@@ -24,7 +33,63 @@ angular.module('starter.services', [])
     };
 })
 
-.service('AzureBlobService', function ($http, $q) {
+.service('AzureClient', function (UI) {
+    var _serviceUrl = 'https://disruptapp.azurewebsites.net';
+    var _authority = "https://login.windows.net/up.ac.th";
+    var _redirectUri = "http://disruptapp.mobile.com";
+    var _resourceUri = "https://graph.windows.net";
+    var _clientId = "eee4b8a2-bf4e-49ed-acc7-ecdf5af43f4f";
+    var _graphApiVersion = "2013-11-08";    
+    var _client;
+
+    var _adalContext;
+    var _adalUser;
+
+    this.getClient = function () {
+        if (!_client) {
+            _client = client = new WindowsAzure.MobileServiceClient(_serviceUrl);
+        }
+
+        return _client;
+    };
+
+    this.getServiceUrl = function () {
+        return _serviceUrl;
+    };
+
+    this.login = function (onSuccess) {
+        _adalContext = new Microsoft.ADAL.AuthenticationContext(_authority);
+
+        _adalContext.tokenCache.readItems().then(function (items) {
+            if (items.length > 0) {
+                _authority = items[0].authority;
+                _adalContext = new Microsoft.ADAL.AuthenticationContext(_authority);
+            }
+
+            _adalContext.acquireTokenSilentAsync(_redirectUri, _clientId).then(function () {
+                _adalContext.tokenCache.readItems().then(function (items2) {
+                    _adalUser = items2[0].userInfo;
+                    onSuccess(_adalUser);
+                });
+            }, function () {
+                _adalContext.acquireTokenAsync(_resourceUri, _clientId, _redirectUri).then(function () {
+                    _adalContext.tokenCache.readItems().then(function (items2) {
+                        _adalUser = items2[0].userInfo;
+                        onSuccess(_adalUser);
+                    });
+                }, function (err) {
+                    UI.error(err);
+                });
+            });
+        });
+    };
+
+    this.getCurrentUser = function () {
+        return _adalUser;
+    };
+})
+
+.service('AzureBlobService', function ($q) {
     var _defer;
     var _remoteFileUrl;
     var _sasToken;
